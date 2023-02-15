d7669f4536eb8689541466ec7927b4265fd55855
Wisly Sousa <wisly.sousa@castgroup.com.br>
2023-02-15 18:34:33 -0300

    feat: adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import {Routes, Route, Navigate} from "react-router-dom"
+export const AppRouter = () => {
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>pagina inicial</p>}/>
+
+            <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+        </Routes>
+    )
+};
