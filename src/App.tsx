3dcb5a1bcd0ff4d1591c038baead475e7ec757b4
Wisly Sousa <wisly.sousa@castgroup.com.br>
2023-02-22 22:34:08 -0300

    fear: adicionando tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+
+import { ThemeProvider } from "@mui/material";
 import {BrowserRouter} from "react-router-dom"
 import { AppRouter } from "./routes";
+import { LightTheme } from "./shared/themes";
+
 export const App = ()=> {
   return (
+    <ThemeProvider theme={LightTheme}>
    <BrowserRouter>
    <AppRouter/>
    </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
