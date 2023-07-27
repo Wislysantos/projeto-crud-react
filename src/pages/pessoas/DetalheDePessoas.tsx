2f6ab938abadb10616f56dafa669467c43c79356
Wislysantos <wislysantos@gmail.com>
2023-07-27 20:15:32 -0300

    feat: add funções do editar e da tela nova

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,30 +1,71 @@
-import { Typography } from "@mui/material"
+import { LinearProgress, Typography } from "@mui/material"
 import { useNavigate, useParams } from "react-router-dom"
 
 
+import { mensagemDeCorfirmacao } from "../../shared/modal";
 import { FerramentaDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePaginaInicial } from "../../shared/layouts";
+import { PessoaServece } from "../../shared/services/api/pessoas/PessoasService";
+import { useEffect, useState } from "react";
 
 export const DetalheDePessoas =()=>{
 
     const {id ='nova'} = useParams<'id'>();
     const navite = useNavigate()
 
+    const [isLoading, setIsLoading] = useState(false)
+    const [nome, setNome] = useState('')
+
+    const handleDelete = (id : number) =>{
+        mensagemDeCorfirmacao('Deseja realmente excluir este item?', ()=>{
+            PessoaServece.deleteById(id)
+            .then((result)=>{
+                if(result instanceof Error){
+                    alert(result.message)
+                }else{
+                    alert('Item excluído com sucesso!')
+                    navite('/listagem-de-pessoas')
+                }
+            }
+                
+            )
+        })
+    }
+    
+    useEffect(()=>{
+        if(id !== 'nova'){
+            setIsLoading(true)
+            PessoaServece.getById(Number(id))
+            .then((result)=>{
+                setIsLoading(false)
+                if(result instanceof Error){
+                    alert(result.message)
+                }else{
+                    console.log(result)
+                    setNome(result.nomeCompleto)
+                }
+           }) 
+        }
+    },[id])
+
+
     return(
         <LayoutBaseDePaginaInicial
-        titulo="Detalhe De Pessoa"
+        titulo={id !== 'nova' ? nome : 'Nova Pessoa'}
         barraDeFerramenta={
             <FerramentaDeDetalhe
                 textoButtonNovo="Nova"
                 mostrarButtonApagar={id !=='nova'}
                 mostrarButtonNovo={id !=='nova'}
 
+                aoClicarApgar={()=>handleDelete(Number(id))}
                 aoClicarNovo={()=>navite(`/pessoas/detalhe/nova`)}
                 aoClicarVoltar={()=>navite(`/listagem-de-pessoas`)}
             ></FerramentaDeDetalhe>
         }
         >
             <Typography variant="h1">detalhe pessoas {id}</Typography>
+            {isLoading && (<LinearProgress variant="indeterminate"/>)}
         </LayoutBaseDePaginaInicial>
     )
 }
